18df5013c57443cc65303dc9924237ef9ec14f7e
Christophe Biocca <christophe.biocca@gmail.com>
2011-12-02 01:29:00 -0500

    Including more logging.

--- server/server.js
+++ server/server.js
@@ -33,14 +33,20 @@ var _ = require('underscore');
 
 everyone.now.getTask = function(retVal){
     // Right now, just return a fake task.
-    retVal(uuid.v4(), "function(k,v,out){out(k,v);}", [{k: 1, v: 2}, {k:22, v:999}]);
+    var taskid = uuid.v4();
+    var code = String(function(k,v,out){out(k,v);});
+    var data = [{k: 1, v: 2}, {k:22, v:999}];
+    console.log('Sent out task #' + taskid);
+    retVal(uuid.v4(), code, data);
 };
 
-everyone.now.completeTask = function(taskid, retVal){
+everyone.now.completeTask = function(taskid, data, retVal){
+    console.log("completed task #" + taskid);
     // Right now, we don't do anything.
     retVal("OK");
 };
 
 everyone.now.heartbeat = function(taskid){
+    console.log("Got heartbeat for task #" + taskid);
     // Right now, we don't do anything.
 };
