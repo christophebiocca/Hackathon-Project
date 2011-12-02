e6701679b7dcfde322b17f42fe02f3ff8be42e9f
Drew Gross <drew.a.gross@gmail.com>
2011-12-02 01:17:08 -0500

    files

--- client/client.js
+++ client/client.js
@@ -1,38 +1,34 @@
 $(document).ready(function () {
     if (!!window.Worker) {
         var worker = new window.Worker("work.js");
+        var currentTaskId;
+        var currentTaskIntervalID;
+        var startNewTask = function () {
+            now.getTask(function (taskId, code, data) {
+                currentTaskId = taskId;
+                worker.postMessage({ 'type': 'NewMapFunction', 'NewFunction': String(code) });
+                worker.postMessage({ 'type': 'NewData', 'NewData': data });
+            });
+            clearInterval(currentTaskIntervalID);
+            currentTaskIntervalID = setInterval(function () {
+                now.heartbeat(currentTaskId);
+            }, 5000);
+        };
         worker.onmessage = function(event){
             if (event.data.type === "DataRequest"){     //if the worker requests data
                 console.log('data requested');          //for debugging
                 
-				//
-				now.ready(function () {						
-                    now.getTask(function (taskId, code, data) {
-                        worker.postMessage({ 'type': 'NewMapFunction', 'NewFunction': String(code) });
-                        worker.postMessage({ 'type': 'NewData', 'NewData': data });
-                    });
-                });
-            }
+				startNewTask();
+            };
             
 			if (event.data.type === "DataReturn"){
                 console.log('data returned');
                 console.log(event.data.Data.k);
                 console.log(event.data.Data.v);
-            }
-        }
-
-
+            };
+        };
         now.ready(function () {
-            var currentTaskId;
-            now.getTask(function (taskId, code, data) {
-                currentTaskId = taskId;
-                worker.postMessage({ 'type': 'NewMapFunction', 'NewFunction': String(code)});
-                worker.postMessage({ 'type': 'NewData', 'NewData': data });
-            });
-
-            setInterval(function () {
-                now.heartbeat(currentTaskId);
-            }, 5000);
+            startNewTask();
         });
     };
 });
