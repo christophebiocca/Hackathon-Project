4fa5f0ec51c83566c71fdebbd4dec9d4b0fd24b8
Christophe Biocca <christophe.biocca@gmail.com>
2011-12-02 01:23:06 -0500

    Fixed an accidental revert.

--- server/server.js
+++ server/server.js
@@ -33,11 +33,7 @@ var _ = require('underscore');
 
 everyone.now.getTask = function(retVal){
     // Right now, just return a fake task.
-    retVal({
-        taskid: uuid.v4(),
-        code: "function(k,v,out){out(k,v);}",
-        data: [{k: 1, v: 2}, {k:22, v:999}]
-    });
+    retVal(uuid.v4(), "function(k,v,out){out(k,v);}", [{k: 1, v: 2}, {k:22, v:999}]);
 };
 
 everyone.now.completeTask = function(taskid, retVal){
