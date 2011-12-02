da9b2e612dfdc6fb847acf2f8ef61c29715ed54f
Drew Gross <drew.a.gross@gmail.com>
2011-12-02 07:36:11 -0500

    merge

--- server/server.js
+++ server/server.js
@@ -23,6 +23,7 @@ app.get('/', function(req, res){
 
 var models = require('./models');
 var mandelbrotstuff = require('./mandelbrot_task.js');
+console.log(mandelbrotstuff);
 mandelbrotstuff.mandelbrot(80);
 app.post('/upload', function(req, res, next){
 
