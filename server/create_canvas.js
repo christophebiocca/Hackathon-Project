4a49e0e11d8fe6bb7475f3251456bba48c6a8709
Akash Vaswani <avaswani@uwaterloo.ca>
2011-12-02 01:15:37 -0500

    changed the range of the function

--- server/create_canvas.js
+++ server/create_canvas.js
@@ -7,8 +7,8 @@
 		c = element.getContext("2d");
 		imageData = c.createImageData(1600, 900);
 		int i=0;
-		for(y=-449; y<=449; y++){
-			for(x=-799; x<=799; x++){
+		for(y=-2; y<2; y+=0.0025){
+			for(x=-3.55; x<3.5; x+=0.0025){
 				color = getColor(x,y)
 				imageData.data[i+0] = color;
 				imageData.data[i+1] = color;
--- server/num_to_color.js
+++ server/num_to_color.js
@@ -5,4 +5,12 @@
 		color = parseInt(color);
 		output(k,color);
 	}
+	
+	var control_experiment = function(x,y){
+		var color = colorVal(x,y)/3921.5;
+		color = parseInt(color);
+		return color;
+	}
+	
 </script>
+lol
