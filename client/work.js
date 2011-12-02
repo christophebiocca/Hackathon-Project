af2625ca7510033ef44634da5c130f539b287bc9
Drew Gross <drew.a.gross@gmail.com>
2011-12-01 20:16:10 -0500

    files

--- client/work.js
+++ client/work.js
@@ -7,14 +7,14 @@ pause = function (millis) {
 }
 
 //the mapper. returns a string represent the result of the processing, which is then returned to the server.
-var mapData = function(dataString){
-    return("no map function");
+var mapData = function (dataString) {
     pause(5000);
+    return("no map function");
 };
 
 self.onmessage = function (event) {
     if (event.data.type === 'NewMapFunction'){
-        eval('mapData = ' + 'function(){pause(5000);' + event.data.NewFunction + '(dataString)');
+        eval('mapData = ' + 'function(){pause(5000);return(' + event.data.NewFunction + '(dataString)});');
         self.postMessage({ 'type': 'DataRequest' });
     };
     if (event.data.type === 'NewData'){
