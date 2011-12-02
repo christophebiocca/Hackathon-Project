78b7ad779ff47265c8be5122bde6924f29536c8d
Drew Gross <drew.a.gross@gmail.com>
2011-12-01 22:06:09 -0500

    files

--- client/client.js
+++ client/client.js
@@ -13,10 +13,9 @@ $(document).ready(function () {
         }
         now.ready(function () {
             now.getTask(function (taskId, code, data) {
-                    worker.postMessage({ 'type': 'NewMapFunction', 'NewFunction': String(code)});
-                    _.each(data, function(datum){
-                        worker.postMessage({ 'type': 'NewData', 'NewData': datum });
-                    });
+                worker.postMessage({ 'type': 'NewMapFunction', 'NewFunction': String(code)});
+                _.each(data, function(datum){
+                    worker.postMessage({ 'type': 'NewData', 'NewData': datum });
                 });
             });
         });
