ffb533559c39bca9e0ebe4e0febdeba18e221c8b
Drew Gross <drew.a.gross@gmail.com>
2011-12-02 01:31:53 -0500

    files

--- client/work.js
+++ client/work.js
@@ -24,11 +24,13 @@ self.onmessage = function (event) {
         self.postMessage({ 'type': 'DataRequest' });
     };
     if (event.data.type === 'NewData'){
+        var results = [];
 		_.each(event.data.NewData, function(datum){
 			mapData(datum.k, datum.v, function (k, v) {
-				self.postMessage({ 'type': 'DataReturn', 'Data': {'k':k,'v':v}});
+                results.push({'k':k,'v':v});
 			});
 		});
+        self.postMessage({'type': 'DataReturn', 'Data': results});
         self.postMessage({ 'type': 'DataRequest' });
     };
 };
