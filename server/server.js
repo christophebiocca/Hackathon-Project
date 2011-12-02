5bf61494b515cdf60c47eb48aa5594c66f844846
Akash Vaswani <avaswani@uwaterloo.ca>
2011-12-02 07:26:41 -0500

    added colorVal to inline function

--- server/server.js
+++ server/server.js
@@ -50,7 +50,32 @@ everyone.now.getTask = function(retVal){
     var taskid = uuid.v4();
 
     var code = String(function(k,v,output){
+val colorVal = function(x,y){
+		
+		var i = 0;
+		var xnew = x;
+		var ynew = y;
+		var xold = x;
+		var yold = x;
+		var value;
+		for(i = 0; i<1000000; ++i){
 
+			xnew = (xold^2) + (yold^2);
+			ynew = 2*xold*yold;
+
+			value = sqrt(xnew^2 + ynew^2);
+
+			if(value>2){
+				return i;
+			}
+
+
+			yold = ynew;
+			xold = xnew;
+		}
+		return 1000000;
+
+	}
 		var color = colorVal(k.x,k.y)/3921.5;
 		color = parseInt(color);
 		output(k,color);
